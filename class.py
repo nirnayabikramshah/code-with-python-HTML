48bf712f14bf9c65f01b821f7939c2f6b8d374bc
Nirnaya Bikram shah <Nirnayabikramshah1@gmail.com>
2026-05-28 06:57:58 +0545

    vote for the school captain

---
+++ class.py
@@ -0,0 +1,9 @@
+r=int(input("ehter the vote in red house "))
+y=int(input("ehter the vote in yellow house "))
+g=int(input("ehter the vote in green house "))
+b=int(input("ehter the vote in blue house "))
+c = r+y+g+b
+if c >= 500:
+    print("you are the school captain")
+else:
+       print("better luck next time ")
