05259b87c83b079971cbfeac80d3fad939325d84
nirnayabikramshah <nirnayabikramshah1@gmail.com>
2026-07-01 20:51:14 +0545

    Made an rose with turtle for hriju

---
+++ rose.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+screen =t.Screen()
+screen.bgcolor("Yellow")  
+t.color("Red")
+t.speed(0)      
+for i in range(150):
+
+    t.forward(i * 1.2)
+    t.left(58)          
+    t.width(i / 50 + 1)
+
+t.done()
